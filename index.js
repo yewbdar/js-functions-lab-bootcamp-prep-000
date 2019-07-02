6360576ba6766743efd1dd95a6c48c425569b4a1
Yewbdar Abera <yewbdar.grima@gmail.com>
2019-07-02 20:00:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // write your code below!
+function happyHolidays(){
+  return "Happy holidays!"
+}
+function happyHolidaysTo(name){
+  return `Happy Holidays,${name}`
+  
+}
+
+function happyHolidaysTo(holiday,name){
+  return `Happy ${holiday},${name}`
+  
+}
