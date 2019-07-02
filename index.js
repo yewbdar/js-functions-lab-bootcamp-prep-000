ace6e99025c3eb29545a356034b9fc87198a2d48
Yewbdar Abera <yewbdar.grima@gmail.com>
2019-07-02 20:10:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function happyHolidayTo(holiday,name){
   return `Happy ${holiday}, ${name}!`
   
 }
-function holidayCountdown(days,holiday){
+function holidayCountdown(holiday,days){
   return `It\'s ${days} days until ${holiday}!`
   
 }
